32cdaed81e025f1647d385f4813210c98cca899c
mdsaif48 <mdsaif21600172@gmail.com>
2025-06-24 17:15:52 +0530

    Create GamewithIntegers.cpp

---
+++ GamewithIntegers.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin>> n;
+        if (n%3!= 0) {
+            cout << "First" << endl;
+            continue;
+      
+        }
+        else{
+            cout << "Second" << endl;
+            continue;
+        }
+
+    }
+
+    return 0;
+}
+    
